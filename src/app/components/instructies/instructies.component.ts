60df3366a14961cff3e10baa2071d75f64af3cfe
bjorn <11500443@student.pxl.be>
2020-02-21 11:21:39 +0100

    dddd
    ddd

--- src/app/components/instructies/instructies.component.ts
+++ src/app/components/instructies/instructies.component.ts
@@ -13,14 +13,77 @@ import {Observable} from "rxjs";
 export class InstructiesComponent implements OnInit {
   keypoint: Keypoint;
   sequences: Observable<Sequence[]>;
+  seq: string = "RH 1 2 4";
+  keypoint1: boolean = true;
+  keypoint2: boolean = true;
+  keypoint3: boolean = true;
+  keypoint4: boolean = true;
+  keypoint5: boolean = true;
+  keypoint6: boolean = true;
+  keypoint7: boolean = true;
+  keypoint8: boolean = true;
+  seqArray: string[] = [];
+  lichaamsdeel : string = "redHand2";
   constructor(private keypointService: KeypointService, private httpService: HttpService) {
     this.keypoint = keypointService.getKeypoint();
     this.httpService.sendKeypoints();
+
   }
   ngOnInit() {
   }
   getSequences(){
     this.sequences = this.httpService.getSequences();
     console.log(this.sequences);
+
+    this.seqArray = this.seq.split(" ");
+    console.log(this.seqArray);
+    let ld = document.getElementById("lichaamsdeell") as HTMLImageElement;
+    switch (this.seqArray[0]) {
+      case "RH":
+        ld.src = "../../../assets/redHand2.png";
+        break;
+      case "LH":
+        ld.src = "../../../assets/blueHand1.png";
+        break;
+      case "RF":
+        ld.src = "../../../assets/redFeet2.png";
+        break;
+      case "LF":
+        ld.src = "../../../assets/blueFeet1.png";
+        break;
+    }
+
+    for (let i = 1; i < this.seqArray.length; i++) {
+      console.log(this.seqArray[i]);
+      let timeout = 2000;
+      setTimeout(() => {
+        switch (this.seqArray[i]){
+        case "1":
+          this.keypoint1 =  false;
+          break;
+        case "2":
+          this.keypoint2 =  false;
+          break;
+        case "3":
+          this.keypoint3 =  false;
+          break;
+        case "4":
+          this.keypoint4 =  false;
+          break;
+        case "5":
+          this.keypoint5 =  false;
+          break;
+        case "6":
+          this.keypoint6 =  false;
+          break;
+        case "7":
+          this.keypoint7 =  false;
+          break;
+        case "8":
+          this.keypoint8 =  false;
+          break;
+      } }, timeout);
+      timeout += 2000;
+    }
   }
 }
